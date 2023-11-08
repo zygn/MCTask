3b2933be44d178674754c84caa14bbb74954c51d
YundoChoi <rikochet@kakao.com>
2023-11-08 16:24:48 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import random
+
+class MCTask:
+
+    def __init__(self, T_HI, T_LO, C, mode):
+        self.T_HI = int(T_HI)
+        self.T_LO = int(T_LO)
+        self.C = int(C)
+        self.mode = mode
+
+
+class MCTaskSet:
+
+    def __init__(self):
+        pass
+
+    def _check_utilization(self, tasks):
+        for task in tasks:
+
+            pass
+
+    def gen_random_task(self):
+        pass
+
+
